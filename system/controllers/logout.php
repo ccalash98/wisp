96f2c82a49f63d1a727c099f3fbafcb8c3ddcbbf
ccalash98 <cristtofher.ccalash@gmail.com>
2024-02-25 02:52:52 -0500

    correcion cierre de sesion

--- system/controllers/logout.php
+++ system/controllers/logout.php
@@ -1,8 +1,17 @@
 <?php
-/**
-* PHP Mikrotik Billing (https://ibnux.github.io/phpmixbill/)
-**/
+
 run_hook('customer_logout'); #HOOK
 if (session_status() == PHP_SESSION_NONE) session_start();
 session_destroy();
-header('location: index.php');
+header('location: index.php?_route=admin/');
+
+
+/*
+run_hook('customer_logout'); #HOOK
+if (session_status() == PHP_SESSION_NONE) session_start();
+session_destroy();
+header('location: login.php'); // Redirige a login.php después de cerrar sesión
+exit(); // Asegura que no se ejecuten más líneas de código después de la redirección
+
+http://localhost/WISP/index.php?_route=admin/
+*/
